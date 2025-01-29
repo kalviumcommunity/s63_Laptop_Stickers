beee0dc90672434f1e7f9dc7356a37f6addd75c4
demo_practice <kartikbhardwaj1111>
2025-01-29 14:23:24 +0530

    Done

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/Ping', (req, res) => {
+    res.send('Pong');
+    });
+
+app.listen(8000, () => {
+    console.log('Server is running on port 8000');
+    });
