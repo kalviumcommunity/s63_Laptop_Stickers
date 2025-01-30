5a2167b065a8d0647340383d7688f7f9bc278842
demo_practice <kartikbhardwaj1111>
2025-01-30 09:41:18 +0530

    successful

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.get('/ping', (req, res) => {
     res.send('pong');
     });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(8000, () => {
+    console.log('Server is running on port 8000');
     });
