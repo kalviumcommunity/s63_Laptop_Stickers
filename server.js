4ac867c5a38076193e7f63f8041e9b8073ae74a2
demo_practice <kartikbhardwaj1111>
2025-02-08 10:54:47 +0530

    Done

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-const express = require('express');
+
+const express = require("express");
+const mongoose = require("mongoose");
+const connectDatabase = require("./database");
+
 const app = express();
+connectDatabase();
 
-app.get('/ping', (req, res) => {
-    res.send('pong');
-    });
+app.get("/ping", (req, res) => {
+  try {
+    res.send("pong");
+  } catch (error) {
+    res.status(500).send("An error occurred");
+  }
+});
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-    });
+// **Add Home Route with DB Status**
+app.get("/", (req, res) => {
+  const status = mongoose.connection.readyState === 1 ? "Connected" : "Not Connected";
+  res.json({ message: "Welcome to the API", db_status: status });
+});
+
+app.listen(8000, () => {
+  console.log(`Server is running on port http://localhost:8000`);
+});
